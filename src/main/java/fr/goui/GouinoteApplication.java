60f71b3f65b44e6c8f5e5ee93e4ecc8dbebdf5f3
goui <guillaume.galliano@gmail.com>
2016-11-22 11:34:17 -0500

    Added spring project

---
+++ src/main/java/fr/goui/GouinoteApplication.java
@@ -0,0 +1,12 @@
+package fr.goui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouinoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GouinoteApplication.class, args);
+	}
+}
