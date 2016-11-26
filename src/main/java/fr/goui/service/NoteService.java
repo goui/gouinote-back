e9eb9eadf97bebe60311299f488791e936e607bb
goui <guillaume.galliano@gmail.com>
2016-11-25 22:14:05 -0500

    Added NoteService with getAllNotes method

---
+++ src/main/java/fr/goui/service/NoteService.java
@@ -0,0 +1,32 @@
+package fr.goui.service;
+
+import fr.goui.dao.NoteRepository;
+import fr.goui.dto.NoteDTO;
+import fr.goui.entity.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The service for everything linked to the notes.
+ */
+@Service
+public class NoteService {
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<NoteDTO> getAllNotes() {
+        List<NoteDTO> notesDTO = new ArrayList<>();
+        List<Note> notes = noteRepository.findAll();
+        notes.forEach(note -> {
+            NoteDTO noteDTO = new NoteDTO();
+            noteDTO.setDate(note.getDate().toInstant().toEpochMilli());
+            noteDTO.setContent(note.getContent());
+            notesDTO.add(noteDTO);
+        });
+        return notesDTO;
+    }
+}
