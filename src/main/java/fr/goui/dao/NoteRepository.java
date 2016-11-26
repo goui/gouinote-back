81fa487c080bedb24dee74912eedaf737e41b447
goui <guillaume.galliano@gmail.com>
2016-11-25 22:13:22 -0500

    Changed interface to JpaRepository to have List instead of Iterable

--- src/main/java/fr/goui/dao/NoteRepository.java
+++ src/main/java/fr/goui/dao/NoteRepository.java
@@ -1,10 +1,10 @@
 package fr.goui.dao;
 
 import fr.goui.entity.Note;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * The data access object for the Note table.
  */
-public interface NoteRepository extends CrudRepository<Note, Long> {
+public interface NoteRepository extends JpaRepository<Note, Long> {
 }
